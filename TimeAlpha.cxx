d9b901d96ead78f959e5f40ec05037d60d6554eb
Katharina von Sturm <katharinavonsturm@gmail.com>
2016-07-11 13:34:29 +0200

    Filling class

--- TimeAlpha.cxx
+++ TimeAlpha.cxx
@@ -4,23 +4,113 @@
 // BAT can be downloaded from http://mpp.mpg.de/bat
 // ***************************************************************
 
+#include <string>
+
 #include "TimeAlpha.h"
 
 #include <BAT/BCMath.h>
 
+#include "TChain.h"
+#include "TH1D.h"
+
+// gerda-ada includes
+#include "FileMap.h"
+#include "DataLoader.h"
+
+using namespace std;
+using namespace gada;
+
 // ---------------------------------------------------------
-TimeAlpha::TimeAlpha(const char * name) : BCModel(name) {
+TimeAlpha::TimeAlpha(const char * name, std::string keylist ) : BCModel(name) {
 	// constructor
 	// define parameters here. For example:
 	// AddParameter("mu",-2,1,"#mu");
 	// and set priors, if using built-in priors. For example:
 	// SetPriorGauss("mu",-1,0.25);
+	ReadData( keylist );
 }
 
 // ---------------------------------------------------------
 TimeAlpha::~TimeAlpha() {
 	// destructor
 }
+
+// ---------------------------------------------------------
+// Read Data from Runs key list tier3
+// ---------------------------------------------------------
+int TimeAlpha::ReadData( string keylist )
+{
+	string GERDA_PHASEII_DATA = getenv("GERDA_PHASEII_DATA");
+
+	//
+	gada::FileMap myMap;
+	myMap.SetRootDir( GERDA_PHASEII_DATA );
+	myMap.BuildFromListOfKeys( keylist );
+
+	gada::DataLoader l;
+	l.AddFileMap(&myMap);
+	l.BuildTier3();
+
+	TChain * chain = l.GetSharedMasterChain();
+	int nentries = chain->GetEntries();
+
+	cout << "There are " << nentries << " events in the chain!" <<endl;
+
+	// fill the data in histograms
+	int eventChannelNumber;
+	unsigned long long timestamp;
+	unsigned int decimalTimestamp;
+	vector<int> * firedFlag = new vector<int>(fNDetectors);
+	int multiplicity;
+	vector<double> * energy = new vector<double>(fNDetectors);
+	int isTP;
+	int isVetoed;
+	int isVetoedInTime;
+	vector<int> * failedFlag = new vector<int>(fNDetectors);
+
+	chain -> SetBranchAddress("eventChannelNumber", &eventChannelNumber);
+	chain -> SetBranchAddress("timestamp",&timestamp);
+	chain -> SetBranchAddress("decimalTimestamp",&decimalTimestamp);
+	chain -> SetBranchAddress("firedFlag", &firedFlag);
+	chain -> SetBranchAddress("multiplicity",&multiplicity);
+	chain -> SetBranchAddress("rawEnergyGauss",&energy);
+	chain -> SetBranchAddress("isVetoed", &isVetoed);
+	chain -> SetBranchAddress("isTP",&isTP);
+	chain -> SetBranchAddress("isVetoedInTime", &isVetoedInTime);
+	chain -> SetBranchAddress("failedFlag",&failedFlag);
+
+	chain->GetEntry( nentries - 1 );
+
+	// Prepare histograms cut the last piece of data if it doen't fit in the 20 days scheme
+	unsigned long secondIn20Days = 60*60*24*20;
+	unsigned long nBins = timestamp / secondIn20Days;
+	int maxDay = 20 * nBins;
+
+	fHTimeAlpha = new TH1D( "HTimeAlpha", "HTimeAlpha", nBins, 0, maxDay );
+	fHLiveTimeFraction = new TH1D( "HLiveTimeFraction", "HLiveTimeFraction", nBins, 0, (double)maxDay );
+
+	for( int e = 0; e < nentries; e++ )
+	{
+		chain->GetEntry(e);
+
+		// Apply cuts
+		if( multiplicity != 1 ) continue;
+		if( isVetoed ) 			continue;
+		if( isVetoedInTime ) 	continue;
+		if( failedFlag ) 		continue;
+
+		if( isTP ) fHLiveTimeFraction->Fill( timestamp / secondIn20Days );
+		else if( energy > 3500. && energy < 5300. ) fHTimeAlpha->Fill( timestamp / secondIn20Days );
+	}
+
+	// Expected number of TP in 20 days: Test Pulser rate is 0.05Hz = 1/20s
+	int TPExpected = 24 * 60 * 60 ;
+
+	fHLiveTimeFraction->Scale( 1./(double)TPExpected );
+
+	return 0;
+}
+
 
 // ---------------------------------------------------------
 double TimeAlpha::LogLikelihood(const std::vector<double> & parameters) {
--- TimeAlpha.h
+++ TimeAlpha.h
@@ -7,6 +7,11 @@
 #ifndef __BAT__TIMEALPHA__H
 #define __BAT__TIMEALPHA__H
 
+#include <cstdlib>
+#include <vector>
+
+#include "TH1D.h"
+
 #include <BAT/BCModel.h>
 
 // This is a TimeAlpha header file.
@@ -17,12 +22,28 @@ class TimeAlpha : public BCModel {
  public:
 
 	// Constructor and destructor
-	TimeAlpha(const char * name = "TimeAlpha");
+	TimeAlpha( const char * name = "TimeAlpha", std::string keylist );
 	~TimeAlpha();
+
+	int ReadData( std::string keylist );
+
+	void SetNDetectors( int n ){ fNDetectors = n; };
+	int GetNDetectors(){ return fNDetectors; };
 
 	// Methods to overload, see file TimeAlpha.cxx
 	double LogLikelihood(const std::vector<double> & parameters);
 	// double LogAPrioriProbability(const std::vector<double> & parameters);
+
+ private:
+
+	int fNDetectors = 40;
+
+	// Data arrays with time distribution of events and livetime fraction in time (counting pulser events)
+	TH1D * fHTimeAlpha;
+	TH1D * fHLiveTimeFraction;
+
+	std::vector<double> fVTimeAlpha;
+	std::vector<double> fVLiveTimeFraction;
 
 };
 // ---------------------------------------------------------

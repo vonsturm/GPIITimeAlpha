b5a22daf5b7d9b1df65d906f6b4cc093a113d8ce
Katharina von Sturm <vonsturm@pd.infn.it>
2017-12-27 15:48:08 +0100

    Added script to run all fits

---
+++ runallTimeAlpha.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./runTimeAlpha enrBEGe-run0054-run0079-worun0069-phy-analysis.txt enrBEGe
+./runTimeAlpha enrCoax-run0054-run0079-worun0060-phy-analysis.txt enrCoax
+./runTimeAlpha natCoax-run0054-run0079-phy-analysis.txt natCoax
+./runTimeAlpha natCoax-run0054-run0079-phy-analysis.txt ANG4
+./runTimeAlpha natCoax-run0054-run0079-phy-analysis.txt GTF45_2
